87fe6f40f470eaacde061d01f66a21cfe7c4f25b
Kash Somani <kash@kashsomani.com>
2023-06-04 14:57:08 -0700

    arrow done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="cont">
+	<section class="two flex flex-col relative">
+		<div class="grid grid-rows-8 h-full w-full gap-0">
+			<div class="row-span-6 grid grid-cols-5 place-items-center w-full mt-20">
+				<div class=" w-full h-full" />
+				<div class=" w-full h-full border-l-4 border-b-4 border-black" />
+				<div class=" w-full h-full" />
+				<div class=" w-full h-full" />
+				<div class=" w-full h-full" />
+			</div>
+			<div class="row-span-2 grid grid-cols-5 place-items-center w-full mb-20">
+				<div class=" w-full h-full" />
+				<div class="  w-full h-full border-r-4 border-black relative">
+					<div class="arrow" />
+				</div>
+				<div class="  w-full h-full" />
+				<div class="  w-full h-full" />
+				<div class="  w-full h-full" />
+			</div>
+		</div>
+	</section>
+	<section class="one">
+		<h1 class="h1 text-9xl">MUTT</h1>
+	</section>
+	<section class="three">
+		<h1>three</h1>
+	</section>
+	<section class="four">
+		<h1>four</h1>
+	</section>
+	<section class="five">
+		<h1>five</h1>
+	</section>
+	<section class="six">
+		<h1>six</h1>
+	</section>
+	<section class="seven">
+		<h1>seven</h1>
+	</section>
+	<section class="eight">
+		<h1>eight</h1>
+	</section>
+</div>
+
+<style>
+	.cont {
+		scroll-snap-type: y mandatory;
+		overflow-y: scroll;
+		overflow-x: hidden;
+		height: 100vh;
+	}
+	section {
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		scroll-snap-align: start;
+	}
+	h1 {
+		color: black;
+	}
+
+	.arrow {
+		width: 50px;
+		height: 50px;
+		border-left: 4px solid black;
+		border-bottom: 4px solid black;
+		transform: rotate(315deg);
+		right: -2px;
+		bottom: 5px;
+		translate: 50%;
+		position: absolute;
+	}
+</style>
