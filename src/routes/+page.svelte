8e75d09a9d9a69c3cb0080f9782e1530f2c4f9f6
Kash Somani <kash@kashsomani.com>
2023-06-04 19:21:31 -0700

    front page restructured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,36 @@
 <div class="cont">
 	<section class="two flex flex-col relative">
 		<div class="grid grid-rows-8 h-full w-full gap-0">
-			<div class="row-span-6 grid grid-cols-5 place-items-center w-full mt-20">
-				<div class=" w-full h-full" />
+			<div
+				class="row-span-6 grid grid-cols-6 place-items-center w-full mt-20 relative gap-0"
+			>
+				<div
+					class=" w-full h-full text-right pr-10 pt-20 bg-red-400 flex flex-col gap-10 items-end text-lg"
+				>
+					<a href="">Work</a>
+
+					<a href="">People</a>
+					<a href="">Howl</a>
+					<a href="">Awards</a>
+					<a href="">Woof</a>
+				</div>
 				<div class=" w-full h-full border-l-4 border-b-4 border-black" />
+				<div
+					class="z-10 t-0 l-0 h-full absolute overlay grid grid-cols-1 place-items-center"
+				>
+					<div class="text-left text-9xl">
+						a production <br />
+						& ad agency <br />
+						mongrel
+					</div>
+				</div>
+				<div class=" w-full h-full border-b-4 border-black" />
 				<div class=" w-full h-full" />
 				<div class=" w-full h-full" />
 				<div class=" w-full h-full" />
 			</div>
-			<div class="row-span-2 grid grid-cols-5 place-items-center w-full mb-20">
+			<div class="row-span-2 grid grid-cols-6 place-items-center w-full mb-20">
+				<div class=" w-full h-full" />
 				<div class=" w-full h-full" />
 				<div class="  w-full h-full border-r-4 border-black relative">
 					<div class="arrow" />
@@ -30,6 +52,7 @@
 	<section class="one">
 		<h1 class="h1 text-9xl">MUTT</h1>
 	</section>
+
 	<section class="three">
 		<h1>three</h1>
 	</section>
@@ -67,7 +90,10 @@
 	h1 {
 		color: black;
 	}
-
+	.overlay {
+		width: 80%;
+		margin-left: 20%;
+	}
 	.arrow {
 		width: 50px;
 		height: 50px;
@@ -78,5 +104,28 @@
 		bottom: 5px;
 		translate: 50%;
 		position: absolute;
+	}
+	a {
+		float: right;
+		text-decoration: none;
+		letter-spacing: 0.15em;
+		width: fit-content;
+		position: relative;
+	}
+	a:after {
+		background: none repeat scroll 0 0 transparent;
+		bottom: 0;
+		content: "";
+		display: block;
+		height: 2px;
+		left: 50%;
+		position: absolute;
+		background: black;
+		transition: width 0.3s ease 0s, left 0.3s ease 0s;
+		width: 0;
+	}
+	a:hover:after {
+		width: 100%;
+		left: 0;
 	}
 </style>
