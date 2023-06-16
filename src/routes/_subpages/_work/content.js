2939f65c9928214473c38d8b343ce407dfb6a087
Kash Somani <kash@kashsomani.com>
2023-06-16 10:08:56 -0700

    no buuttons

--- src/routes/_subpages/_work/content.js
+++ src/routes/_subpages/_work/content.js
@@ -1,42 +1,42 @@
 export const content = {
 	tide: {
 		video:
-			"https://player.vimeo.com/video/833333393?h=33a7e99454&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/833333393?background=1&h=33a7e99454&title=0&byline=0&portrait=0",
 		text: "The goal was simple: to win the Super Bowl. We knew Tide was the number one detergent brand in America. That insight alone made us realize that being at the top probably meant the majority of the clothes featured in other ads would have been washed with it. The clients played a vital role in finding the perfect brand partners within Procter to bring the idea to life in the best possible way. The rest is history.",
 	},
 	got2be: {
 		video:
-			"https://player.vimeo.com/video/393728356?h=23ec0ab010",
+			"https://player.vimeo.com/video/393728356?background=1&h=23ec0ab010",
 		text: "Göt2b is a hair wax brand. They needed to showcase the versatility of their product by featuring an array of hairstyles.  So we created eye-catching and humorous sketches where heads in the bathroom, each with a different hairstyle, engaged in lively conversations to convince their indecisive owner to pick one for the day.",
 	},
 	olay: {
 		video:
-			"https://player.vimeo.com/video/268997798?h=f6f3a295c4&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/268997798?background=1&h=f6f3a295c4&title=0&byline=0&portrait=0",
 		text: "Olay wanted to create an idea that helped fight ageism. We came up with something simple, yet powerful. When people compliment someone adding “for your age” at the end, the compliment instantly becomes an insult. We did a real photoshoot with women of all ages, taking a picture right after a compliment was said and again after adding “for your age” at the end. The result is impressive. ",
 	},
 	neom: {
 		video:
-			"https://player.vimeo.com/video/684252200?h=24a0cf8bcd&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/684252200?background=1&h=24a0cf8bcd&title=0&byline=0&portrait=0",
 		text: "Neom, the green mega enterprise being built in the Middle East, aimed to introduce a previously unimaginable activity to local tourists: skiing in the desert. We crafted an extraordinary story in which skiers gracefully glided not down, but up the mountain and into the sky.",
 	},
 	verizon: {
 		video:
-			"https://player.vimeo.com/video/313249817?h=0aa6d5d1ef&autoplay=1&loop=1&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/313249817?background=1&h=0aa6d5d1ef&autoplay=1&loop=1&title=0&byline=0&portrait=0",
 		text: "Every year, 40,000 Americans owe their lives to first responders and millions of first responders rely on the network.  We told the stories of a special few who had been saved — 11 NFL players and a head coach. Their stories became a 12-part film series, 30-minute CBS documentary. The film series became the most talked about SuperBowl campaign and solidified Verizon’s reputation as the most reliable network.",
 	},
 	bellagio: {
 		video:
-			"https://player.vimeo.com/video/569883025?h=031db795a3&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/569883025?background=1&h=031db795a3&title=0&byline=0&portrait=0",
 		text: "For Bellagio’s first rebranding in over a decade, we needed to convey the premium exultation of a visit. There are a few moments in life when we look around and find ourselves amidst a purely delightful experience. Some of those rare moments can be found at Bellagio.",
 	},
 	donateLife: {
 		video:
-			"https://player.vimeo.com/video/348841980?h=220b13c0df&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/348841980?background=1&h=220b13c0df&title=0&byline=0&portrait=0",
 		text: "People die every day because there are not enough organ donors. We rewarded organ donors who had committed a traffic violation with a ‘2nd Chance Ticket’ as a hidden camera stunt with real police departments to raise awareness. Organ donations went way up, lives were saved, awards won.",
 	},
 	metaverse: {
 		video:
-			"https://player.vimeo.com/video/348841980?h=220b13c0df&title=0&byline=0&portrait=0",
+			"https://player.vimeo.com/video/348841980?background=1&h=220b13c0df&title=0&byline=0&portrait=0",
 		text: 'How does the agency who hasn’t done a Metaverse project remain relevant in the burgeoning space and retain and attract clients? Answer: Deliver a best in category complex innovative experience for Cannes featuring the agency’s best work across a multitude of clients on an insane five week timeline. Check it out, it’s a nice place to visit: <a href="https://web.journee.live/mwverse" style="text-decoration: underline;">web.journee</a>. ',
 	},
 }
