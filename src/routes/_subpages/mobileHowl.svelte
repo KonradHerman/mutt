9dae00cd4da708cad6e11d58e22551d41c1b5b5d
Konrad <klherman@umass.edu>
2023-07-13 22:10:54 -0400

    mobilehowl

---
+++ src/routes/_subpages/mobileHowl.svelte
@@ -0,0 +1,8 @@
+<div class="h-full w-full overflow-scroll">
+    <div class="sticky top-20 grid align-center background-white ">
+      <p class="pl-2 text-5xl lg:text-8xl text-center all-round-gothic-medium-oblique uppercase spacing ">
+        howl
+      </p>
+    </div>
+  </div>
+  
--- src/routes/_subpages/mobilehero.svelte
+++ src/routes/_subpages/mobilehero.svelte
@@ -1,42 +1,7 @@
-
-<style lang="postcss" scoped>
-	.center {
-		@apply row-span-6 grid grid-cols-6 place-items-center w-full relative gap-0 h-full;
-	}
-	.motto {
-		letter-spacing: 0.4em;
-		@apply w-full h-5/6 flex flex-col justify-end font-bold;
-	}
-	.overlay {
-		width: 80%;
-		margin-left: 20%;
-	}
-	.arrow {
-		width: 50px;
-		height: 50px;
-		border-left: 4px solid white;
-		border-bottom: 4px solid white;
-		transform: rotate(315deg);
-		right: -2px;
-		bottom: 5px;
-		translate: 50%;
-		position: absolute;
-	}
-	.title {
-		letter-spacing: 0.1em;
-	}
-	@media (max-width: 768px) {
-		.grid-query {
-			/* Change the grid to a single column on small screens. */
-			grid-template-columns: 1fr;
-			justify-items: stretch;
-			align-items: stretch;
-		}
-	}
-	@media (max-width: 1024px) {
-		.arrow {
-			width: 25px;
-			height: 25px;
-		}
-	}
-</style>
+<div class="z-50 all-round-gothic text-center text-4xl text-white">
+  <div class="h-full w-full">
+    <p class="p-8">an ad agency</p>
+    <p class="p-8">& production</p>
+    <p class="p-8">mongrel</p>
+  </div>
+</div>
